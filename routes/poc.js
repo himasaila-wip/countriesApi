84dce7572e31e26ceaca7bd9b9a73e6bce96a560
GO20184107 <gonugunta.himasaila@wipro.com>
2021-03-29 16:03:17 +0530

    COuntryAPI updated

--- routes/poc.js
+++ routes/poc.js
@@ -9,60 +9,60 @@ const agent = new https.Agent({
     
 });
 
-//using direct headers
-router.get('/getAccessToken1', function(req, res, next) {
-    axios.get("https://www.universal-tutorial.com/api/getaccesstoken",{
-        headers:{   "Accept": "application/json",
-                    "api-token": "u1krKJJUp5o9T9Qng93u86sOpPaFxJWCLGyaG5RxSm-w6QFGEo48u08IlbrqvfWoLYI",
-                    "user-email": "suma2@gmail.com"
-                },
-                //"proxy": "http://<server>:3000",
-                rejectUnauthorized: false,
-                ejectUnauthorized: false,
-                requestCert: false,
-                agent: false,
-            }).then((response) => {
-                res.json(response.data)
-            })
-            .catch((error) => {
-                  console.error(error)
-                })
-            console.log("hi");
+// //using direct headers
+// router.get('/getAccessToken1', function(req, res, next) {
+//     axios.get("https://www.universal-tutorial.com/api/getaccesstoken",{
+//         headers:{   "Accept": "application/json",
+//                     "api-token": "u1krKJJUp5o9T9Qng93u86sOpPaFxJWCLGyaG5RxSm-w6QFGEo48u08IlbrqvfWoLYI",
+//                     "user-email": "suma2@gmail.com"
+//                 },
+//                 //"proxy": "http://<server>:3000",
+//                 rejectUnauthorized: false,
+//                 ejectUnauthorized: false,
+//                 requestCert: false,
+//                 agent: false,
+//             }).then((response) => {
+//                 res.json(response.data)
+//             })
+//             .catch((error) => {
+//                   console.error(error)
+//                 })
+//             console.log("hi");
             
-});
+// });
 
-//using direct headers
-router.get('/countries1', function(req, res, next) {
-    request({
-        method: 'GET',
-        uri: ' https://www.universal-tutorial.com/api/countries/',
-        headers:{   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzdW1hMkBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJ1MWtyS0pKVXA1bzlUOVFuZzkzdTg2c09wUGFGeEpXQ0xHeWFHNVJ4U20tdzZRRkdFbzQ4dTA4SWxicnF2ZldvTFlJIn0sImV4cCI6MTYxNTk3NDQ4Mn0.bOaygW4el7qUat7vATVpTLekgoadTf7GED0hLzMv7FU",
-                    "Accept": "application/json"
-                },
-                 rejectUnauthorized: false,
-                 ejectUnauthorized: false,
-                 requestCert: false,
-                 agent: false,
-            }).on('error',error =>{
-                res.status(404).send(error.message);
-            }).pipe(res);
-});
+// //using direct headers
+// router.get('/countries1', function(req, res, next) {
+//     request({
+//         method: 'GET',
+//         uri: ' https://www.universal-tutorial.com/api/countries/',
+//         headers:{   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzdW1hMkBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJ1MWtyS0pKVXA1bzlUOVFuZzkzdTg2c09wUGFGeEpXQ0xHeWFHNVJ4U20tdzZRRkdFbzQ4dTA4SWxicnF2ZldvTFlJIn0sImV4cCI6MTYxNTk3NDQ4Mn0.bOaygW4el7qUat7vATVpTLekgoadTf7GED0hLzMv7FU",
+//                     "Accept": "application/json"
+//                 },
+//                  rejectUnauthorized: false,
+//                  ejectUnauthorized: false,
+//                  requestCert: false,
+//                  agent: false,
+//             }).on('error',error =>{
+//                 res.status(404).send(error.message);
+//             }).pipe(res);
+// });
 
-//using direct headers
-router.get('/states/:states1', function(req, res, next) {
-    states= req.params['states'];
-    request({
-        method: 'GET',
-        uri: ' https://www.universal-tutorial.com/api/states/'+states+'/',
-        headers:{   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzdW1hMkBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJ1MWtyS0pKVXA1bzlUOVFuZzkzdTg2c09wUGFGeEpXQ0xHeWFHNVJ4U20tdzZRRkdFbzQ4dTA4SWxicnF2ZldvTFlJIn0sImV4cCI6MTYxNTk3NDQ4Mn0.bOaygW4el7qUat7vATVpTLekgoadTf7GED0hLzMv7FU",
-                    "Accept": "application/json"
-                },
-                 rejectUnauthorized: false,
-                 ejectUnauthorized: false,
-                 requestCert: false,
-                 agent: false,
-            }).pipe(res);
-});
+// //using direct headers
+// router.get('/states/:states1', function(req, res, next) {
+//     states= req.params['states'];
+//     request({
+//         method: 'GET',
+//         uri: ' https://www.universal-tutorial.com/api/states/'+states+'/',
+//         headers:{   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJzdW1hMkBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJ1MWtyS0pKVXA1bzlUOVFuZzkzdTg2c09wUGFGeEpXQ0xHeWFHNVJ4U20tdzZRRkdFbzQ4dTA4SWxicnF2ZldvTFlJIn0sImV4cCI6MTYxNTk3NDQ4Mn0.bOaygW4el7qUat7vATVpTLekgoadTf7GED0hLzMv7FU",
+//                     "Accept": "application/json"
+//                 },
+//                  rejectUnauthorized: false,
+//                  ejectUnauthorized: false,
+//                  requestCert: false,
+//                  agent: false,
+//             }).pipe(res);
+// });
 
 
 //getting geaders from postman
